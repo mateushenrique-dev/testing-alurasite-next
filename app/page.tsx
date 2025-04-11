5fabf8a2d1142d035d1e13b6dcbdece615cd2a99
Mateus Henrique <mhenriqq32@gmail.com>
2025-04-11 19:28:16 -0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 export default async function Home() {
-  const response = await fetch('https://api.thecatapi.com/v1/images/search');
+  const response = await fetch('https://api.thecatapi.com/v1/images/search', {
+    cache: 'force-cache'
+  });
   const data = await response.json();
   const imageUrl = data[0].url;
 
