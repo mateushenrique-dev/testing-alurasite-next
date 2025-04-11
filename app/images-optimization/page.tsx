921b7963325b56fcfd20e3fd44c977f6e3dff985
Mateus Henrique <mhenriqq32@gmail.com>
2025-04-11 20:54:20 -0300

    .

--- app/images-optimization/page.tsx
+++ app/images-optimization/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function ImagesOptimization() {
   return (
     <div>
-      <Image src="/cat.jpg" width={200} height={200} alt="" />
+      <Image src="/cat.jpg" width={500} height={375} alt="" />
       <img src="/cat.jpg" alt="" />
     </div>
   );
